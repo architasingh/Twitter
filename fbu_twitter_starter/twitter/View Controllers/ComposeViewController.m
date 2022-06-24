a57a2997e1bc7c146722f97ac8a2f24cbdd7cbed
architasingh <49347272+architasingh@users.noreply.github.com>
2022-06-24 14:49:08 -0700

    Added character limit feature

--- fbu_twitter_starter/twitter/View Controllers/ComposeViewController.m
+++ fbu_twitter_starter/twitter/View Controllers/ComposeViewController.m
@@ -19,11 +19,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.text.delegate = self;
 }
 
-- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
-{
-    return self.text.text.length + (self.text.text.length - range.length) <= 140;
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+
+    // Set the max character limit
+    int characterLimit = 140;
+
+    // Construct what the new text would be if we allowed the user's latest edit
+    NSString *newText = [self.text.text stringByReplacingCharactersInRange:range withString:text];
+
+    // Should the new text should be allowed? True/False
+    return newText.length < characterLimit;
 }
 
  - (IBAction)makeATweet:(id)sender {
@@ -32,8 +40,12 @@
                  NSLog(@"Error composing Tweet: %@", error.localizedDescription);
              }
              else{
-               [self.delegate didTweet:tweet];
-                NSLog(@"Compose Tweet Success!");
+                 if ([self textView:self.text shouldChangeTextInRange:NSMakeRange(0, 140) replacementText:self.text.text]) {
+                     [self.delegate didTweet:tweet];
+                      NSLog(@"Compose Tweet Success!");
+                 } else {
+                     NSLog(@"Exceeded character limit");
+                 }
             }
      }];
  }
